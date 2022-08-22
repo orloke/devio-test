2e56a468f24b83b32276172363b3c1a847048b14
Junior Dering <juniordering@hotmail.com>
2022-08-22 18:33:22 -0400

    feat: responsive header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,43 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 import { DivHeader } from './styles';
 import logo from '../../../public/images/fastfood-logo.png';
 
 export function Header() {
+  const [clickButton, setClickButton] = useState('');
+
   return (
     <DivHeader>
       <div>
         <Image src={logo} alt="logo" width={60} height={60} />
         <nav>
           <Link href="/">
-            <button type="button">Pedidos</button>
+            <button
+              className={clickButton === 'pedido' ? 'active' : ''}
+              onClick={() => setClickButton('pedido')}
+              type="button"
+            >
+              Pedidos
+            </button>
           </Link>
           <Link href="/">
-            <button type="button">Cozinha</button>
+            <button
+              onClick={() => setClickButton('cozinha')}
+              className={clickButton === 'cozinha' ? 'active' : ''}
+              type="button"
+            >
+              Cozinha
+            </button>
           </Link>
           <Link href="/">
-            <button type="button">Retirada</button>
+            <button
+              onClick={() => setClickButton('retirada')}
+              className={clickButton === 'retirada' ? 'active' : ''}
+              type="button"
+            >
+              Retirada
+            </button>
           </Link>
         </nav>
       </div>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,40 @@
 import styled from 'styled-components';
 
+interface props {
+  backgroundButton: boolean;
+}
+
 export const DivHeader = styled.div`
   background-color: green;
+
+  div {
+    max-width: 90%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem 0;
+    @media (max-width: 768px) {
+      flex-direction: column;
+      align-items: center;
+      gap: 1rem;
+    }
+    nav {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      button {
+        background-color: transparent;
+        border: none;
+        color: white;
+        font-weight: bold;
+        padding: 0.5rem;
+        cursor: pointer;
+      }
+
+      .active {
+        background-color: #184d18;
+        border-radius: 10px;
+      }
+    }
+  }
 `;
