5d2883f6fbc87344c436e4774be07b4cd478aa3b
Junior Dering <juniordering@hotmail.com>
2022-09-12 16:00:09 -0400

    fix: deploy Vercel

--- src/store/products/index.ts
+++ src/store/products/index.ts
@@ -2,93 +2,93 @@ import { createSlice } from '@reduxjs/toolkit';
 import { Additional, Finished, Market, Produto } from '../../types';
 
 const initialState = {
-	show: false,
-	product: {} as Produto,
-	additional: [] as Additional[],
-	market: [] as Market[],
-	finished: [] as Finished[],
-	delivery: [] as Finished[],
+  show: false,
+  product: {} as Produto,
+  additional: [] as Additional[],
+  market: [] as Market[],
+  finished: [] as Finished[],
+  delivery: [] as Finished[],
 };
 
 const productsSlice = createSlice({
-	name: '@products',
-	initialState,
-	reducers: {
-		setChangeModal(state, action) {
-			state.show = action.payload;
-		},
-		takeProductModal(state, action) {
-			state.product = action.payload;
-		},
-		addAdditioanl(state, action) {
-			if (action.payload.title !== '') {
-				Object.assign(state, {
-					additional: [...state.additional, action.payload],
-				});
-			}
-		},
-		removeAdditioanl(state, action) {
-			if (action.payload === 'removeAll') {
-				state.additional = [];
-			}
-			const a = state.additional.filter(
-				item => item.title !== action.payload.title,
-			);
-			state.additional = a;
-		},
-		addProductMarket(state, action) {
-			Object.assign(state, {
-				market: [...state.market, action.payload],
-			});
-		},
-		deleteProductMarket(state, action) {
-			if (action.payload === 'removeAll') {
-				state.market = [];
-			}
-			const a = state.market.filter(
-				item => item.product.id !== action.payload.id,
-			);
-			state.market = a;
-		},
-		addForPayment(state, action) {
-			const marketFinishedId = {
-				...action.payload,
-				id: state.finished.length + 1,
-			};
-			Object.assign(state, {
-				finished: [...state.finished, marketFinishedId],
-			});
-		},
-		deleteForPayment(state, action) {
-			const a = state.finished.filter(item => item.id !== action.payload.id);
-			state.finished = a;
-		},
-		addForDelivery(state, action) {
-			const addDelivery = {
-				...action.payload,
-				id: state.delivery.length + 1,
-			};
-			Object.assign(state, {
-				delivery: [...state.delivery, addDelivery],
-			});
-		},
-		deleteForDelivery(state, action) {
-			const a = state.delivery.filter(item => item.id !== action.payload.id);
-			state.delivery = a;
-		},
-	},
+  name: '@products',
+  initialState,
+  reducers: {
+    setChangeModal(state, action) {
+      state.show = action.payload;
+    },
+    takeProductModal(state, action) {
+      state.product = action.payload;
+    },
+    addAdditioanl(state, action) {
+      if (action.payload.title !== '') {
+        Object.assign(state, {
+          additional: [...state.additional, action.payload],
+        });
+      }
+    },
+    removeAdditioanl(state, action) {
+      if (action.payload === 'removeAll') {
+        state.additional = [];
+      }
+      const a = state.additional.filter(
+        item => item.title !== action.payload.title,
+      );
+      state.additional = a;
+    },
+    addProductMarket(state, action) {
+      Object.assign(state, {
+        market: [...state.market, action.payload],
+      });
+    },
+    deleteProductMarket(state, action) {
+      if (action.payload === 'removeAll') {
+        state.market = [];
+      }
+      const a = state.market.filter(
+        item => item.product.id !== action.payload.id,
+      );
+      state.market = a;
+    },
+    addForPayment(state, action) {
+      const marketFinishedId = {
+        ...action.payload,
+        id: state.finished.length + 1,
+      };
+      Object.assign(state, {
+        finished: [...state.finished, marketFinishedId],
+      });
+    },
+    deleteForPayment(state, action) {
+      const a = state.finished.filter(item => item.id !== action.payload.id);
+      state.finished = a;
+    },
+    addForDelivery(state, action) {
+      const addDelivery = {
+        ...action.payload,
+        id: state.delivery.length + 1,
+      };
+      Object.assign(state, {
+        delivery: [...state.delivery, addDelivery],
+      });
+    },
+    deleteForDelivery(state, action) {
+      const a = state.delivery.filter(item => item.id !== action.payload.id);
+      state.delivery = a;
+    },
+  },
 });
 
 export const {
-	setChangeModal,
-	takeProductModal,
-	addAdditioanl,
-	removeAdditioanl,
-	addProductMarket,
-	deleteProductMarket,
-	addForPayment,
-	deleteForPayment,
-	addForDelivery,
-	deleteForDelivery,
+  setChangeModal,
+  takeProductModal,
+  addAdditioanl,
+  removeAdditioanl,
+  addProductMarket,
+  deleteProductMarket,
+  addForPayment,
+  deleteForPayment,
+  addForDelivery,
+  deleteForDelivery,
 } = productsSlice.actions;
 export default productsSlice.reducer;
