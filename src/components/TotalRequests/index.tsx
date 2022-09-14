5860ea53d0c3d1f20db1c6467799f4d82a52134c
Junior Dering <juniordering@hotmail.com>
2022-09-14 14:50:32 -0400

    feat: totalRequests component

---
+++ src/components/TotalRequests/index.tsx
@@ -0,0 +1,19 @@
+import { formatValue } from '../../helps';
+import { Market } from '../../types';
+import { Container } from './styles';
+
+interface PropsTotalRequests {
+  market: Market[];
+}
+
+export default function TotalRequests({ market }: PropsTotalRequests) {
+  const totalRequests = market.reduce((acc, crr) => acc + crr.product.total, 0);
+
+  const newTotalRequests = formatValue(totalRequests);
+  return (
+    <Container>
+      <h6>Total do pedido:</h6>
+      <h5>{newTotalRequests}</h5>
+    </Container>
+  );
+}
---
+++ src/components/TotalRequests/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  width: 100%;
+  h6 {
+    margin-top: 0.5rem;
+    font-size: 1.2rem;
+  }
+  h5 {
+    margin-left: auto;
+    margin-bottom: 0;
+    display: flex;
+    align-items: center;
+  }
+`;
